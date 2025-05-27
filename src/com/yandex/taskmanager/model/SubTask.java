ce7d3b88d091d597012fce6671320292cf490a85
Anton Riabushkin <riabushkinanton@yandex.ru>
2025-05-27 22:55:37 +0700

    fix: изменить epicId на private final int в SubTask.

--- src/com/yandex/taskmanager/model/SubTask.java
+++ src/com/yandex/taskmanager/model/SubTask.java
@@ -1,19 +1,19 @@
 package com.yandex.taskmanager.model;
 
 public class SubTask extends Task {
-    private final Integer epicId;
+    private final int epicId;
 
-    public SubTask(String name, String description, Integer epicId) {
+    public SubTask(String name, String description, int epicId) {
         super(name, description);
         this.epicId = epicId;
     }
 
-    public SubTask(int id, String name, String description, Integer epicId) {
+    public SubTask(int id, String name, String description, int epicId) {
         super(id, name, description);
         this.epicId = epicId;
     }
 
-    public SubTask(int id, String name, String description, Status status, Integer epicId) {
+    public SubTask(int id, String name, String description, Status status, int epicId) {
         super(id, name, description, status);
         this.epicId = epicId;
     }
