b4265141af6fd5940bf23c94978092141dda874b
Anton Ryabushkin <riabushkinanton@yandex.ru>
2025-07-08 20:19:33 +0300

    feat: подготовить адаптеры GsonAdapters.

---
+++ src/com/yandex/taskmanager/json/GsonAdapters.java
@@ -0,0 +1,71 @@
+package com.yandex.taskmanager.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.google.gson.stream.JsonToken;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.Duration;
+import java.time.format.DateTimeFormatter;
+
+public class GsonAdapters {
+
+    public static Gson createGson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .registerTypeAdapter(Duration.class, new DurationAdapter())
+                .setPrettyPrinting()
+                .serializeNulls()
+                .create();
+    }
+
+    private static class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+        private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd--MM--yyyy HH:mm");
+
+        @Override
+        public void write(final JsonWriter jsonWriter, final LocalDateTime localDateTime) throws IOException {
+            if (localDateTime == null) {
+                jsonWriter.nullValue();
+            } else {
+                jsonWriter.value(localDateTime.format(dtf));
+            }
+        }
+
+        @Override
+        public LocalDateTime read(final JsonReader jsonReader) throws IOException {
+            if (jsonReader.peek() == JsonToken.NULL) {
+                jsonReader.nextNull();
+                return null;
+            }
+            return LocalDateTime.parse(jsonReader.nextString(), dtf);
+        }
+    }
+
+    private static class DurationAdapter extends TypeAdapter<Duration> {
+        @Override
+        public void write(JsonWriter jsonWriter, Duration duration) throws IOException {
+            if (duration == null) {
+                jsonWriter.nullValue();
+            } else {
+                jsonWriter.value(duration.toString());
+            }
+        }
+
+        @Override
+        public Duration read(JsonReader jsonReader) throws IOException {
+            if (jsonReader.peek() == JsonToken.NULL) {
+                jsonReader.nextNull();
+                return null;
+            }
+            return Duration.parse(jsonReader.nextString());
+        }
+    }
+}
+
+
+
+
