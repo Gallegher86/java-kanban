689aa36c6c931441340e7ba0b3afe1f0e7d73475
Anton Ryabushkin <riabushkinanton@yandex.ru>
2025-07-09 21:04:53 +0300

    fix: разделить отправку ответов в BaseHttpHandler на json и text.

--- src/com/yandex/taskmanager/web/BaseHttpHandler.java
+++ src/com/yandex/taskmanager/web/BaseHttpHandler.java
@@ -8,24 +8,41 @@ import java.nio.charset.StandardCharsets;
 
 public class BaseHttpHandler {
     protected void sendText(HttpExchange exchange, String text) throws IOException {
-        sendResponse(exchange, text, 200);
+        sendResponseWithJson(exchange, text, 200);
+    }
+
+    protected void sendCreated(HttpExchange exchange, String text) throws IOException {
+        sendResponseWithJson(exchange, text, 201);
     }
 
     protected void sendNotFound(HttpExchange exchange, String text) throws IOException {
-        sendResponse(exchange, text, 404);
+        sendResponseWithText(exchange, text, 404);
     }
 
     protected void sendHasInteractions(HttpExchange exchange, String text) throws IOException {
-        sendResponse(exchange, text, 409);
+        sendResponseWithText(exchange, text, 409);
     }
 
     protected void sendMethodNotAllowed(HttpExchange exchange, String text) throws IOException {
-        sendResponse(exchange, text, 405);
+        sendResponseWithText(exchange, text, 405);
     }
 
-    private void sendResponse(HttpExchange exchange, String text, int code) throws IOException {
+    protected void sendInvalidPathFormat(HttpExchange exchange, String text) throws IOException {
+        sendResponseWithText(exchange, text, 400);
+    }
+
+    private void sendResponseWithJson(HttpExchange exchange, String text, int code) throws IOException {
         byte[] resp = text.getBytes(StandardCharsets.UTF_8);
         exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        exchange.sendResponseHeaders(code, resp.length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(resp);
+        }
+    }
+
+    private void sendResponseWithText(HttpExchange exchange, String text, int code) throws IOException {
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        exchange.getResponseHeaders().add("Content-Type", "text/plain;charset=utf-8");
         exchange.sendResponseHeaders(code, resp.length);
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(resp);
