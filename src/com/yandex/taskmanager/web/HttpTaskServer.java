ad87b54b14f25db3eb963efff836242197e25111
Anton Ryabushkin <riabushkinanton@yandex.ru>
2025-07-12 22:10:34 +0300

    feat: добавить HttpTaskServer.

--- src/com/yandex/taskmanager/web/HttpTaskServer.java
+++ src/com/yandex/taskmanager/web/HttpTaskServer.java
@@ -9,15 +9,19 @@ import java.net.InetSocketAddress;
 
 public class HttpTaskServer {
     private static final int PORT = 8080;
-    private final TaskManager manager;
     private final HttpServer httpServer;
 
     public HttpTaskServer(TaskManager manager) {
-        this.manager = manager;
         try {
             httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't start the server at PORT: " + PORT);
+            httpServer.createContext("/tasks", new TasksHandler(manager));
+            httpServer.createContext("/epics", new EpicsHandler(manager));
+            httpServer.createContext("/subtasks", new SubTasksHandler(manager));
+            httpServer.createContext("/history", new HistoryHandler(manager));
+            httpServer.createContext("/prioritized", new PrioritizedHandler(manager));
+            httpServer.createContext("/all", new AllTasksHandler(manager));
+        } catch (IOException ex) {
+            throw new RuntimeException("Can't start the server at PORT: " + PORT, ex);
         }
     }
 
@@ -28,10 +32,10 @@ public class HttpTaskServer {
 
     public void stop() {
         httpServer.stop(0);
-        System.out.println("HTTP server is stopped");
+        System.out.println("HTTP server is stopped.");
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         TaskManager manager = Managers.getDefaultTaskManager();
         HttpTaskServer server = new HttpTaskServer(manager);
         server.start();
