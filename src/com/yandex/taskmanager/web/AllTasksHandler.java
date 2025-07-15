64c4cec63c61a2796c08061911fd96dfc639cfa2
Anton Ryabushkin <riabushkinanton@yandex.ru>
2025-07-15 13:45:20 +0300

    feat: добавить POST к AllTasksHandler.

--- src/com/yandex/taskmanager/web/AllTasksHandler.java
+++ src/com/yandex/taskmanager/web/AllTasksHandler.java
@@ -3,6 +3,7 @@ package com.yandex.taskmanager.web;
 import com.yandex.taskmanager.model.Epic;
 import com.yandex.taskmanager.model.SubTask;
 import com.yandex.taskmanager.model.Task;
+import com.yandex.taskmanager.model.TaskType;
 import com.yandex.taskmanager.service.TaskManager;
 import com.yandex.taskmanager.web.dto.TaskDto;
 
@@ -29,26 +30,36 @@ class AllTasksHandler extends BaseHttpHandler implements HttpHandler {
     public void handle(HttpExchange httpExchange) throws IOException {
         try {
             String method = httpExchange.getRequestMethod();
-            String path = httpExchange.getRequestURI().getPath();
-            String[] parts = path.split("/");
+            String rawPath = httpExchange.getRequestURI().getPath();
+            String path = rawPath.replaceAll("/+$", "");
 
             switch (method) {
                 case "GET":
-                    if (parts.length == 2) {
+                    if (path.matches("^/all$")) {
                         sendAllTasks(httpExchange);
-                    } else if (parts.length == 3) {
-                        int id = parseId(parts);
+                    } else if (path.matches("^/all/\\d+$")) {
+                        int id = parseId(path);
                         sendAnyTask(httpExchange, id);
                     } else {
                         sendInvalidPathFormat(httpExchange, "Bad request: wrong path format");
                     }
                     break;
+                case "POST":
+                    if (path.matches("^/all$")) {
+                        createAnyTask(httpExchange);
+                    } else if (path.matches("^/all/\\d+$")) {
+                        int id = parseId(path);
+                        updateAnyTask(httpExchange, id);
+                    } else {
+                        sendInvalidPathFormat(httpExchange, "Bad request: wrong path format");
+                    }
+                    break;
                 case "DELETE":
-                    if (parts.length == 2) {
+                    if (path.matches("^/all$")) {
                         manager.deleteAllTasks();
                         sendOk(httpExchange, "All tasks deleted.");
-                    } else if (parts.length == 3) {
-                        int id = parseId(parts);
+                    } else if (path.matches("^/all/\\d+$")) {
+                        int id = parseId(path);
                         manager.deleteAnyTaskById(id);
                         sendOk(httpExchange, "Task with id: " + id + " deleted.");
                     } else {
@@ -101,5 +112,84 @@ class AllTasksHandler extends BaseHttpHandler implements HttpHandler {
                 .collect(Collectors.toList());
 
         sendText(httpExchange, gson.toJson(dtoList));
+    }
+
+    private void createAnyTask(HttpExchange httpExchange) throws IOException {
+        String jsonResponse;
+
+        try {
+            TaskDto dto = readDto(httpExchange, gson);
+            if (dto == null) {
+                sendInvalidPathFormat(httpExchange, "Empty or malformed JSON, can't create Task.");
+                return;
+            }
+
+            if (dto.getTaskType() == null) {
+                throw new IllegalArgumentException("'taskType' is required to create Task.");
+            }
+
+            switch (dto.getTaskType()) {
+                case TaskType.TASK:
+                    Task newTask = manager.createTask(TaskDto.toNewTask(dto));
+                    jsonResponse = gson.toJson(TaskDto.fromTask(newTask));
+                    sendCreated(httpExchange, jsonResponse);
+                    break;
+                case TaskType.EPIC:
+                    Epic newEpic = manager.createEpic(TaskDto.toNewEpic(dto));
+                    jsonResponse = gson.toJson(TaskDto.fromEpic(newEpic));
+                    sendCreated(httpExchange, jsonResponse);
+                    break;
+                case TaskType.SUBTASK:
+                    SubTask newSubTask = manager.createSubTask(TaskDto.toNewSubTask(dto));
+                    jsonResponse = gson.toJson(TaskDto.fromSubTask(newSubTask));
+                    sendCreated(httpExchange, jsonResponse);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported task type: " + dto.getTaskType());
+            }
+        } catch (IllegalArgumentException ex) {
+            sendHasInteractions(httpExchange, ex.getMessage());
+        }
+    }
+
+    private void updateAnyTask(HttpExchange httpExchange, int id) throws IOException {
+        try {
+            TaskDto dto = readDto(httpExchange, gson);
+            if (dto == null) {
+                sendInvalidPathFormat(httpExchange, "Empty or malformed JSON, can't create Task.");
+                return;
+            }
+
+            if (dto.getTaskType() == null) {
+                throw new IllegalArgumentException("'taskType' is required to create Task.");
+            }
+
+            switch (dto.getTaskType()) {
+                case TaskType.TASK:
+                    Task taskToUpdate = TaskDto.toTaskWithId(dto, id);
+                    manager.updateTask(taskToUpdate);
+                    sendOk(httpExchange, "Task with id: " + id + " updated.");
+                    break;
+                case TaskType.EPIC:
+                    String name = dto.getName();
+                    String description = dto.getDescription();
+
+                    Epic epicToUpdate = new Epic(id, name, description);
+                    manager.updateEpic(epicToUpdate);
+                    sendOk(httpExchange, "Epic with id: " + id + " updated.");
+                    break;
+                case TaskType.SUBTASK:
+                    SubTask subTaskToUpdate = TaskDto.toSubTaskWithId(dto, id);
+                    manager.updateSubTask(subTaskToUpdate);
+                    sendOk(httpExchange, "SubTask with id: " + id + " updated.");
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported task type: " + dto.getTaskType());
+            }
+        } catch (IllegalArgumentException ex) {
+            sendHasInteractions(httpExchange, ex.getMessage());
+        } catch (NotFoundException ex) {
+            sendNotFound(httpExchange, ex.getMessage());
+        }
     }
 }
